d883862814578dbfbbec7cb99c5206dac9812ba0
timothy <megbeckim@gmail.com>
2018-04-22 17:29:48 -0400

    two HeadRooms, one for album catalog and one for photo catalog (used refs) - keeps scroll position on "back"; needs serious styling

--- src/Site.js
+++ src/Site.js
@@ -10,6 +10,8 @@ import album from './album1.js';
 export class Site extends Component {
     constructor(props) {
         super(props);
+        this.myRef = React.createRef();
+        this.myRef2 = React.createRef();
         this.state = {
             };
     }
@@ -22,17 +24,32 @@ export class Site extends Component {
         this.setState({selectedAlbum: null});
     }
 
+    componentDidMount() {
+        this.setState( { myRef: this.myRef.current, myRef2: this.myRef2.current } );
+    }
+
     render() {
         return (
             <div className='site'>
-                <Headroom>
-                    <Menu selectedAlbum={ this.state.selectedAlbum } onUnselectAlbum={ this.unselectAlbum.bind(this) }/>
-                </Headroom>
-                <div className={ classNames('album-catalog-container', { open: !this.state.selectedAlbum }) }>
-                    <AlbumCatalog onAlbumSelected={ this.selectAlbum.bind(this) }/>
+                <div className={ classNames('album-selector', { open: !this.state.selectedAlbum }) }>
+                    <div className='album-selector-container' ref={this.myRef}>
+                        <Headroom disable={ !this.state.myRef } parent={ () => this.myRef.current }>
+                            <Menu selectedAlbum={ this.state.selectedAlbum } onUnselectAlbum={ this.unselectAlbum.bind(this) }/>
+                        </Headroom>
+                        <div className='album-catalog-container'>
+                            <AlbumCatalog onAlbumSelected={ this.selectAlbum.bind(this) }/>
+                        </div>
+                    </div>
                 </div>
-                <div className={ classNames('thumbnails-container', { open: this.state.selectedAlbum }) }>
-                    <Thumbnails album={album}/>
+                <div className={ classNames('photo-selector', { open: this.state.selectedAlbum }) }>
+                    <div className='photo-selector-container' ref={this.myRef2}>
+                        <Headroom disable={ !this.state.myRef2 } parent={ () => this.myRef2.current }>
+                            <div style={{ height: "2em" }} onClick={ this.unselectAlbum.bind(this) }>back</div>
+                        </Headroom>
+                        <div className='thumbnails-container'>
+                            <Thumbnails album={album}/>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
