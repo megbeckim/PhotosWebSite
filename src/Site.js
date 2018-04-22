a0b4650a18af5e4eca7785e0ded913e76da4a1c6
timothy <megbeckim@gmail.com>
2018-04-22 19:14:59 -0400

    refactored to consistent naming

--- src/Site.js
+++ src/Site.js
@@ -10,8 +10,8 @@ import album from './album1.js';
 export class Site extends Component {
     constructor(props) {
         super(props);
-        this.myRef = React.createRef();
-        this.myRef2 = React.createRef();
+        this.albumCatalogRef = React.createRef();
+        this.albumRef = React.createRef();
         this.state = {
             };
     }
@@ -25,35 +25,35 @@ export class Site extends Component {
     }
 
     componentDidMount() {
-        this.setState( { myRef: this.myRef.current, myRef2: this.myRef2.current } );
+        this.setState( { albumCatalogRef: this.albumCatalogRef.current, albumRef: this.albumRef.current } );
     }
 
     componentDidUpdate(prevProps, prevState) {
         // TODO this is always true at the moment b/c selectedAlbum is set back to null upon returning to albums
         if ( prevState.selectedAlbum !== this.state.selectedAlbum ) {
-            this.myRef2.current.scrollTop = 0;
+            this.albumRef.current.scrollTop = 0;
         }
     }
 
     render() {
         return (
             <div className='site'>
-                <div className={ classNames('album-selector', { open: !this.state.selectedAlbum }) }>
-                    <div className='album-selector-container' ref={this.myRef}>
-                        <Headroom disable={ !this.state.myRef } parent={ () => this.myRef.current }>
+                <div className={ classNames('album-catalog-container', { open: !this.state.selectedAlbum }) }>
+                    <div className='album-catalog' ref={this.albumCatalogRef}>
+                        <Headroom disable={ !this.state.albumCatalogRef } parent={ () => this.albumCatalogRef.current }>
                             <Menu selectedAlbum={ this.state.selectedAlbum } onUnselectAlbum={ this.unselectAlbum.bind(this) }/>
                         </Headroom>
-                        <div className='album-catalog-container'>
+                        <div className='album-catalog-wrapper'>
                             <AlbumCatalog onAlbumSelected={ this.selectAlbum.bind(this) }/>
                         </div>
                     </div>
                 </div>
-                <div className={ classNames('photo-selector', { open: this.state.selectedAlbum }) }>
-                    <div className='photo-selector-container' ref={this.myRef2}>
-                        <Headroom disable={ !this.state.myRef2 } parent={ () => this.myRef2.current }>
+                <div className={ classNames('album-container', { open: this.state.selectedAlbum }) }>
+                    <div className='album' ref={this.albumRef}>
+                        <Headroom disable={ !this.state.albumRef } parent={ () => this.albumRef.current }>
                             <div style={{ height: "2em" }} onClick={ this.unselectAlbum.bind(this) }>back</div>
                         </Headroom>
-                        <div className='thumbnails-container'>
+                        <div className='album-wrapper'>
                             <Thumbnails album={album}/>
                         </div>
                     </div>
