33801bb62db77f02d40cbcc5ce79fe5aa9088752
timothy <megbeckim@gmail.com>
2018-04-30 20:21:35 -0400

    added caption; added blurred background for photos

--- src/photo/Photo.js
+++ src/photo/Photo.js
@@ -10,8 +10,10 @@ export class Photo extends Component {
         const photoIx = this.props.photoIx;
         const picture = this.props.album.chapters[photoIx.chapterIx].pictures[photoIx.photoIx];
         return (
-            <div className='photo-container'>
-                <img src={`${this.props.album.folder}/${picture.fileName}`} onClick={ this.props.unselectPhoto } />
+            <div className='photo-container' onClick={ this.props.unselectPhoto } >
+                <img className='background' src={`${this.props.album.folder}/${picture.fileName}`} />
+                <img className='photo' src={`${this.props.album.folder}/${picture.fileName}`} />
+                <div className='caption'>{ picture.caption }</div>
             </div>
         );
     }
