1d7bb5d4e88ff424d0ab0bb41ff9324cc5dcbaeb
timothy <megbeckim@gmail.com>
2018-09-16 19:46:54 -0400

    removed log messages

--- src/recreateHistory.js
+++ src/recreateHistory.js
@@ -2,7 +2,6 @@ import { homeRoute, albumRoute, photoRoute, mapRoute } from './routes';
 
 export default function recreateHistory() {
     const hash = window.location.hash;
-    console.log('hash', window.location.hash);
 
     const albumRegex = new RegExp('^#' + albumRoute('([^/]+)') + '$');
     const photoRegex = new RegExp('^#' + photoRoute('([^/]+)','(.+)') + '$');
@@ -15,20 +14,15 @@ export default function recreateHistory() {
     let newHistory;
 
     if (photoMatch) {
-        console.log('photo match', photoMatch);
         newHistory = [ '#' + homeRoute(), '#' + albumRoute(photoMatch[1]), hash ];
     } else if (albumMatch) {
-        console.log('album match', albumMatch);
         newHistory = [ '#' + homeRoute(), hash ];
     } else if (mapMatch) {
-        console.log('map match', mapMatch);
         newHistory = [ '#' + homeRoute(), hash ];
     } else {
-        console.log('no match');
         newHistory = [ hash ];
     }
 
-    console.log('new history is', newHistory);
     const [ first, ...rest ] = newHistory;
 
     if (first !== hash) {
